a1dacd91a6e6ee8463dfb761b7fd4a05c18671bd
li.chengjin <li.chengjin@immomo.com>
2022-06-29 10:36:45 +0800

    dev

---
+++ src/main/java/com/example/javatest/JavaTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.javatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication.run(JavaTestApplication.class, args);
+
+        System.out.println(888888);
+    }
+
+}
